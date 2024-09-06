7afe28d0ff7912e31a88e6a957717533c325e5cc
lastpwnd <last.pwnd@gmail.com>
2024-09-06 02:54:12 -0400

    password-validator-hot-fix

--- src/middleware/auth.js
+++ src/middleware/auth.js
@@ -4,7 +4,7 @@ const { StatusCodes } = require('http-status-codes')
 
 const registerValidationRules = [
     body('email', "Wrong email format, valid example: youremail@mailservice.com").trim().notEmpty().isEmail().isLength({ max: 50 }),
-    body('password', "Required length is 4 to 30. Use only [A-Z, a-z, 0-9]").trim().isAlphanumeric().isLength({min: 4, max: 30}),
+    body('password', "Required length is 4 to 30. Use only [A-Z, a-z, 0-9, !?@#$%^&*~_]").trim().matches(/^[a-z0-9!?@#$%^&*~_]+$/i).isLength({min: 4, max: 30}),
     body('firstName', "Required length is 3 to 20. Use only [A-Z, a-z]").trim().isAlpha().isLength({min: 3, max: 20}),
     body('lastName', "Required length is 3 to 20. Use only [A-Z, a-z]").trim().isAlpha().isLength({min: 3, max: 20}),
     body('userName', "Required length is 3 to 20. Use only [A-Z, a-z, 0-9]").trim().matches(/^[a-z0-9?^]+$/i).isLength({min: 3, max: 20}).optional({nullable: true, checkFalsy: true})
@@ -12,7 +12,7 @@ const registerValidationRules = [
 
 const loginValidationRules = [
     body('email', "Wrong email format, valid example: youremail@mailservice.com").trim().notEmpty().isEmail().isLength({ max: 50 }),
-    body('password', "Required length is 4 to 30. Use only [A-Z, a-z, 0-9]").trim().isAlphanumeric().isLength({min: 4, max: 30})
+    body('password', "Required length is 4 to 30. Use only [A-Z, a-z, 0-9, !?@#$%^&*~_]").trim().matches(/^[a-z0-9!?@#$%^&*~_]+$/i).isLength({min: 4, max: 30})
 ]
 
 const authValidation = (req, res, next) => {
