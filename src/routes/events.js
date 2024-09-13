2b5c337e859b459bf8e6d9d426bff97fef09efc6
lastpwnd <last.pwnd@gmail.com>
2024-09-12 21:36:01 -0400

    minor-changes

--- src/routes/events.js
+++ src/routes/events.js
@@ -1,7 +1,7 @@
 const express = require('express')
 const router = express.Router()
 const { authValidation } = require('../middleware/auth')
-const { eventValidationRules } = require('../middleware/event')
+const { eventValidationRules, eventUpdateValidationRules } = require('../middleware/event')
 const { validateErrorHandler } = require('../middleware/validationErrorHandler')
 const { getAllEvents, getEvent, createEvent, deleteEvent, updateEvent } = require('../controllers/events')
 
@@ -9,7 +9,7 @@ router
     .get('/', getAllEvents)
     .get('/:id', getEvent)
     .post('/', authValidation, eventValidationRules, validateErrorHandler, createEvent)
-    .put('/:id', authValidation, eventValidationRules, validateErrorHandler, updateEvent)
+    .put('/:id', authValidation, eventUpdateValidationRules, validateErrorHandler, updateEvent)
     .delete('/:id', authValidation, deleteEvent)
 
 module.exports = router
